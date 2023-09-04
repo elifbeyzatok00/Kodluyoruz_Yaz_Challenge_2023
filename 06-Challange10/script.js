a7e1e812c0240c562014080be98ec2fabc6d208c
elifbeyzatok00 <elifbeyzatok@gmail.com>
2023-09-04 21:16:20 +0300

    commit challange10

--- 06-Challange10/script.js
+++ 06-Challange10/script.js
@@ -7,6 +7,21 @@
 */
 
 //Easy
+// Bir dizi tanımla
+const sayilar = [10, 5, 25, 30, 15, 40, 20];
+
+// Dizinin ilk elemanını en büyük olarak kabul et
+let enBuyukSayi = sayilar[0];
+
+// Dizi içinde dolaşarak en büyük sayıyı bul
+for (let i = 1; i < sayilar.length; i++) {
+  if (sayilar[i] > enBuyukSayi) {
+    enBuyukSayi = sayilar[i];
+  }
+}
+
+// Sonucu ekrana yazdır
+console.log("Dizinin en büyük sayısı:", enBuyukSayi);
 
 //Medium
 
